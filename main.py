5596fea0aabc2ad7489121d1609a0ff2e03876a5
AlanNaoto <naoto321@gmail.com>
2018-10-20 13:09:15 -0300

    Secoes com blocos para desenvolver diferentes funcoes criadas

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+Arquivo principal para o programa criado durante o hackathon Nasa SpaceApp
+Comentários dos autores
+a
+b
+c
+'''
+
+# Funcao 1 #
+# Leitura dos arquivos de entrada
+# Pre-processamento (tratar dados vazios, por exemplo)
+# Separar dados para treino, teste e validacao (70, 15, 15)
+
+# Funcao 2 #
+# Criacao do modelo de classificacao (arvore de decisao)
+
+# Funcao 3 #
+# Criacao do arquivo de saida do programa
+
+if __name__ == "__main__":
+    print('ok')
+
