8f47fec0c6a00422e5b0fded4ecba69fb2a6f1c9
Alan <naoto321@gmail.com>
2018-10-21 01:17:04 -0300

    apaguei o lixo do leitura dados

--- Script/LeituraDados.py
+++ Script/LeituraDados.py
@@ -6,23 +6,6 @@
 
 class LeituraDados:
     def __init__(self):
-        import pandas as pd
-        from sklearn.model_selection import train_test_split
+        
 
-        # Leitura do arquivo csvv
-        NomeArquivoCSV = "RequisitosPlantinhas - Theo Bruno e Alan.csv"
-        Dados = pd.read_csv(NomeArquivoCSV)
-
-        # Leitura de labels e features
-        ColunaLabel = ["Nome"]
-        ColunasFeatures = ["Temp Min", "Temp Max", "Exposicao a luz solar", "Umidade relativa do ar (%)"]
-        DadosLabel = Dados.loc[:, ColunaLabel]
-        DadosFeatures = Dados.loc[:, ColunasFeatures]
-
-        # Separa entre treino e teste
-        XTrain, XTest, YTrain, YTest = train_test_split(DadosLabel, DadosFeatures, test_size=0.2, random_state=0)
-
-        # Aplica pre-processamento das features
-        oi = YTest.values.ravel()
-        print(oi)
-        pass
+dic = {'abacate':[ [1, 2], [5, 7] ], [[1, 5], [8, 9]]}
